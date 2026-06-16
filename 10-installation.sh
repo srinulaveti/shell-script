b6b5f28aff92119a65f484e7e092f7eb4c06b97d
srinu <srinulaveti01.com>
2026-06-16 18:16:22 +0530

    1st script

--- 10-installation.sh
+++ 10-installation.sh
@@ -3,15 +3,14 @@ uid=$(id -u)
 if [ $uid -ne 0 ]
 then 
 echo  " run this script with root user"
-exit 0
+exit 1
 else 
 echo "its rootuser"
-exit 1
 fi
 
 dnf install mysql -y
 
-if [ $? eq 0 ]
+if [ $? -eq 0 ]
 then 
 echo " my sql installed sucesss"
 
@@ -21,12 +20,11 @@ if
 
 dnf install git -y
 
-if [ $? eq 0 ]
+if [ $? -eq 0 ]
 then 
 echo " git installed sucessfully srinu"
-exit 0
+exit 1
 else
 echo " git failed srinu"
-exit 1
 fi
 
