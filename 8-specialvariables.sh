dc45e80e1b8e3eb873addb9aa209e204e60fd8cf
srinu <srinulaveti01.com>
2026-06-16 17:24:42 +0530

    1st script

---
+++ 8-specialvariables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+sleep 60 &
+echo "Process ID of last background command: $!"
+echo "the next command is true are false o or 1 to exit command :$?"
+sleep 60 &
+echo "the next command is true are false o or 1 to exit command :$?"
