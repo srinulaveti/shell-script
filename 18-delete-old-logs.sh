a94d624e306f4ba6851b710c24822109a25a1599
srinu <srinulaveti01.com>
2026-06-28 11:24:38 +0530

    function call from 2nd script

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,27 @@
+##!/bin/bash
+user=(id -u)
+dir=/var/log/sree
+R="\e[31"
+G="\e[32"
+Y="\e[33"
+
+if [ $user -ne 0]
+then
+echo " run the script with root user "
+exit 1
+else
+echo " you are the root user "
+fi 
+if [ -d $dir "if exist "]
+then 
+echo "$dir is exist already "
+else
+echo "$dir u can check again correct one "
+fi
+File=$(find $dir -name *.log* -mtime +10)
+do while IFS= read -r line
+echo " print $line"
+done
+
+
+
