b364bed169b171acf2697590d0005c7c14877093
srinu <srinulaveti01.com>
2026-06-28 10:50:46 +0530

    function call from 2nd script

---
+++ set.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+user=$(id -u)
+fun(){
+    echo " failed at $1 : $2 "
+
+    trap 'fun {$LINENO} "$BASH_COMMAND"'ERR
+}
+if [$user -ne o]
+then 
+echo " run the script with root user "
+exit 1
+else
+echo " u r super user proceed "
+
+dnf install wget -y 
+echo " it is working srinu "
