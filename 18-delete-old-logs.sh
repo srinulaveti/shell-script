f3bb57f45701dd1263f48344c72b87e1b115544c
srinu <srinulaveti01.com>
2026-06-28 11:32:51 +0530

    function call from 2nd script

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -5,21 +5,22 @@ R="\e[31"
 G="\e[32"
 Y="\e[33"
 
-if [ $user -ne 0]
+if [ $user -ne 0 ]
 then
 echo " run the script with root user "
 exit 1
 else
 echo " you are the root user "
 fi 
-if [ -d $dir "if exist "]
+if [ -d $dir "if exist " ]
 then 
 echo "$dir is exist already "
 else
-echo "$dir u can check again correct one "
+echo " $dir u can check again correct one "
 fi
-File=$(find $dir -name *.log* -mtime +10)
-do while IFS= read -r line
+File=$(find $dir -name "*.log*" -mtime +10)
+ while IFS= read -r line
+do 
 echo " print $line"
 done
 
